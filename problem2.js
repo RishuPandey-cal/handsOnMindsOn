cd8368f3570c48b41e99e5bdc9df633244e4f252
RishuPandey-cal <rishu.pandey@calsoftinc.com>
2025-03-18 10:56:13 +0530

    Create problem2.js
    problem2 solution

---
+++ problem2.js
@@ -0,0 +1,52 @@
+function generateString(limit) {
+    let S = '';
+    let num = 1;
+    while (S.length < limit) {
+        S += num.toString();
+        num++;
+    }
+    return S;
+}
+
+
+function findOccurrences(n, S) {
+    let positions = [];
+    let str_n = n.toString();
+    let idx = 0;
+
+
+    while ((idx = S.indexOf(str_n, idx)) !== -1) {
+        positions.push(idx + 1); 
+        idx++; 
+    }
+
+    return positions;
+}
+
+
+function solve() {
+    
+    const limit = 9000000; 
+    const S = generateString(limit);
+
+   
+    let resultSum = 0;
+
+   
+    for (let k = 1; k <= 13; k++) {
+        const n = Math.pow(3, k);
+        const occurrences = findOccurrences(n, S);
+        if (occurrences.length >= 3 ** k) {
+            resultSum += occurrences[3 ** k - 1];  
+        }
+    }
+
+    return resultSum;
+}
+
+console.log(solve());
+
+
+
+
+
