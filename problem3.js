716239156b2769f4f97034f6d7fa58fe06d506ae
RishuPandey-cal <rishu.pandey@calsoftinc.com>
2025-03-18 10:57:48 +0530

    Create problem3.js
    problem 3 solution

---
+++ problem3.js
@@ -0,0 +1,11 @@
+function computeG_optimized(n) {
+    return (n ** 3) / 72 + (n ** 2) / 48 + (n) / 144;
+}
+
+function formatScientific(num) {
+    return num.toExponential(10).replace("e", "e");
+}
+
+let n = 1e11;
+let result = computeG_optimized(n);
+console.log(formatScientific(result));
